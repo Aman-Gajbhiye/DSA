8905d98bd3b8c596d4560ff078dc38e2a4228e57
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-15 14:02:26 +0530

    6 created

---
+++ lecture_4/6_grade_card_program.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n<=59){
+        cout<<"fail";
+    }
+    else if(n>=60 && n<=69){
+        cout<<"D";
+    }
+    else if(n>=70 && n<=79){
+        cout<<"C";
+    }
+    else if(n>=80 && n<=89){
+        cout<<"B";
+    }
+    else if(n>=90 && n<=100){
+        cout<<"A";
+    }
+    else{
+        cout<<"invalid";
+    }
+}
