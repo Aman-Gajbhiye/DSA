6476905d9599a3886e3fd43fbd84f227f6fa2e2d
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-16 14:29:29 +0530

    updated

--- lecture 5/pattern_6.cpp
+++ lecture 5/pattern_6.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main(){
      int n;
      cin>>n;
-
+     char str;
+     str = ' ';
      for (int i = 1;i <= n;i++){
          for (int j = 1;j <= i-1;j++){
-         cout<<"    ";}
+         cout<<" ";
+        }
      for (int j = 1; j <= n-i+1; j++){
           cout<<"* ";
           }
