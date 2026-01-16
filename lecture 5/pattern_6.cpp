ffc493da9afc0fc0ba7bdbdac030bb13a4fbb384
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-16 14:42:40 +0530

    u

--- lecture 5/pattern_6.cpp
+++ lecture 5/pattern_6.cpp
@@ -6,8 +6,8 @@ int main(){
      char str;
      str = ' ';
      for (int i = 1;i <= n;i++){
-         for (int j = 1;j <= i-1;j++){
-         cout<<"    ";
+         for (int j = 1;j <= 2*i-2;j++){
+         cout<<"  ";
         }
      for (int j = 1; j <= n-i+1; j++){
           cout<<"* ";
