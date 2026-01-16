00d488a4354f0ed0fec388bfc9bbdab524ef68f4
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-16 14:05:55 +0530

    12 updated

--- lecture_4/12_lottery_game.cpp
+++ lecture_4/12_lottery_game.cpp
@@ -5,21 +5,9 @@ int main(){
     cin>>n;
     if(n>=300 && n<=460){
         cout<<"you won a Macbook";
-        if(n>=300 && n<=380){
-            cout<<"Model:M1 mac";
-        }
-        else{
-            cout<<"Model:M2 mac";
-        }
     }
     else if(n>=200 && n<=280){
         cout<<"Kurkure";
-        if(n>=300 && n<=380){
-            cout<<"Model:M1 mac";
-        }
-        else{
-            cout<<"Model:M2 mac";
-        }
     }
     else if(n>=1100 && n<=1500){
         cout<<"Cycle";
