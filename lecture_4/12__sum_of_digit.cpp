944c78ffab21311b509f7a9ba6df704fc191cf5e
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-17 22:39:59 +0530

    12 created

---
+++ lecture_4/12__sum_of_digit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int temp = num;
+
+    while (temp != 0) {
+        int digit = temp % 10; 
+        sum += digit;          
+        temp /= 10;            
+    }
+
+    cout << "Sum of digits of " << num << " = " << sum;
+
+    return 0;
+}
