e45f8ce623cd6c686b2c2f73180d7c9325419bad
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-25 00:29:00 +0530

    1 created

---
+++ code_blocks_assesment/pattern_1.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    int n = (m+1)/2;
+for(int i = 1; i<=n; i++){
+    //outer space
+    for(int j = 1; j <= n-i; j++){
+        cout<<"*\t";
+    }
+    cout<<"*\t";
+    //inner space 
+    for(int j = 1;j<=2*i-3;j++){
+        cout<<" \t";
+    }
+    //star
+    if(i == 1){
+        for(int j = 1; j <= n-i; j++){
+        cout<<"*\t";
+    }
+}
+    else{
+        for(int j = 1; j <= n-i+1; j++){
+        cout<<"*\t";
+    }
+    
+    }
+
+    cout<<endl;
+}
+for(int i = n-1; i>=1; i--){
+    //outer space
+    for(int j = 1; j <= n-i; j++){
+        cout<<"*\t";
+    }
+    cout<<"*\t";
+    //inner space 
+    for(int j = 1;j<=2*i-3;j++){
+        cout<<"\t";
+    }
+    //star
+    if(i == 1){
+        for(int j = 1; j <= n-i; j++){
+        cout<<"*\t";
+    }
+}
+    else{
+        for(int j = 1; j <= n-i+1; j++){
+        cout<<"*\t";
+    }
+    
+    }
+
+    cout<<endl;
+}
+
+    return 0;
+}
