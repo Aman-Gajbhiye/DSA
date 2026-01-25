068286a7ba6e787c26b187e37675e92de4459b6f
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-01-25 22:16:14 +0530

    updated

--- lecture_10/tempCodeRunnerFile.cpp
+++ lecture_10/tempCodeRunnerFile.cpp
@@ -1,3 +1,17 @@
-        //   for(int j = i-1; j>= 1 ;j--){
-        //     cout<<j<<" \t";
-        // }
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a = 1; 
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n-i; j++){
+            cout<<"  \t";
+        }
+        for(int j = 1; j<=2*i - 1 ;j++){
+            cout<<a<<" \t";
+            a++;
+        }
+    cout<<endl;
+    }
+}
