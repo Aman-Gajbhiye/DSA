c4c5996848d477e7be33fa9ba4914f664001e730
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-04 22:17:28 +0530

    deleted

--- lecture_7/tempCodeRunnerFile.cpp
+++ lecture_7/tempCodeRunnerFile.cpp
@@ -1,19 +1,23 @@
+#include<iostream>
+using namespace std;
+// total pattern , 1,3,5,7,9 = 2i - 1
 int main(){
     int n;
     cin>>n;
-    for(int i = 1;i<=n;i++){
-        for(int j = 1; j <= i; j++){
-            cout<<"* ";
+    for(int i = 1;i <= n;i++){
+        //spcaes
+        for(int j = 1; j <= n-i;j++){
+            cout<<"  ";
+        }
+        for(int j = 1; j<=2*i-1;j++){
+            if(j%2 == 0){
+                cout<<"! ";
+            }
+            else{
+                cout<<"* ";
+            }
         }
         cout<<endl;
     }
-
-    for(int i = n-1;i>=1;i--){
-        for(int j = 1; j <= i; j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-
-
+    
 }
