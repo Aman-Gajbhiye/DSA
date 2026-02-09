f7b33a2041507bc7bec6787c7aa599bb5dc2ada5
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-09 14:34:50 +0530

    c

--- lecture_18/3_min_num.cpp
+++ lecture_18/3_min_num.cpp
@@ -5,15 +5,15 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    int max = INT_MIN;
+    int min = INT_MAX;
     int s = sizeof(arr)/sizeof(int);
     for(int i = 0; i < s; i++){
         cin>>arr[i];
     }
     for(int i = 0; i < s; i++){
-        if(arr[i]>max){
-            max = arr[i];
+        if(arr[i]<min){
+            min = arr[i];
         }
     }
-    cout<<max;
+    cout<<min;
 }
