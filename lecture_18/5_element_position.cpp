d69d166b268383db32a804b12695661a0256f18f
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-10 15:32:35 +0530

    u

--- lecture_18/5_element_position.cpp
+++ lecture_18/5_element_position.cpp
@@ -8,6 +8,7 @@ int main(){
     int n1;
     cout<<"number to find ";
     cin>>n1;
+    int flag = 0;
    int arr[n];
    int s = sizeof(arr)/sizeof(int);
    for(int i = 0; i < s; i++){
@@ -16,6 +17,11 @@ int main(){
    for(int i = 0; i <= s; i++){
     if(arr[i] == n1){
         cout<<i+1;
+        flag = 1;
     }
    }
+   if(flag == 0){
+    cout<<"not found";
+   }
+   
 }
