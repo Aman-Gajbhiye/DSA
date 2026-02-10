ae532ab6e2b2f6605272eb31f7a8165dbd77be26
Aman-Gajbhiye <amangajbhiye120@gmail.com>
2026-02-10 15:55:57 +0530

    c

--- lecture_19/1_vectors_basic.cpp
+++ lecture_19/1_vectors_basic.cpp
@@ -4,14 +4,24 @@ using namespace std;
 int main(){
     //initialization
     vector<int> a;
+    //update
     a.push_back(1);
     a.push_back(2);
     a.push_back(3);
     a.push_back(4);
     a.push_back(5);
+    //size
     cout<<a.size()<<endl;
+    // capacity
     cout<<a.capacity()<<endl;
+    // delete
+    a.pop_back();
     for(int i = 0; i <= a.size(); i++){
-        cout<<a[i]<<endl;
+        cout<<a[i]<<" ";
     }
+    cout<<endl;
+    // front element
+    cout<<a.front()<<endl;
+    // last element
+    cout<<a.back()<<endl;
 }
